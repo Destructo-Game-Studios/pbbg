dc7fdb660a59178a1fe91768bfc700e20e39a30c
Travis Weston <tweston2013@gmail.com>
2021-01-14 18:31:05 -0500

    Create basic Activity code that returns a reward if an ability check succeeds, or 0 if it fails

--- src/Activity.php
+++ src/Activity.php
@@ -2,10 +2,17 @@
 
 namespace Destructo;
 
+use Destructo\Abilities\Ability;
 use Destructo\Money;
 
 class Activity {
-    public function do() : Money {
-        return new Money(100);
+    public function do(Ability $ability, int $target, int $reward) : Money {
+
+        if(!$ability->abilityCheck( $target )) {
+            return new Money(0);
+        }
+
+        return new Money($reward);
+
     }
 }
--- tests/Activities/ActivityTest.php
+++ tests/Activities/ActivityTest.php
@@ -2,15 +2,38 @@
 
 namespace DestructoTest\Activities;
 
+use Destructo\Abilities\Ability;
 use Destructo\Activity;
 use Destructo\Money;
 
 class ActivityTest extends \PHPUnit\Framework\TestCase {
     public function testActivityReturnsMoneyObject() {
         $activity = new Activity();
-        $result = $activity->do();
+        $ability = new Ability('strength', 0);
+
+        $result = $activity->do($ability, 0, 100);
 
         $this->assertInstanceOf(Money::class, $result);
+    }
+
+    public function testActivityReturnsZeroMoneyIfAbilityCheckFails() {
+        $ability = new Ability('strength', 0);
+
+        $activity = new Activity();
+        
+        $money = $activity->do($ability, 1000, 100);
+
+        $this->assertEquals(0, $money->amount);
+    }
+
+    public function testActivityReturnsCorrectMoneyIfAbilityCheckSucceeds() {
+        $ability = new Ability('strength', 100);
+
+        $activity = new Activity();
+        
+        $money = $activity->do($ability, 0, 100);
+
+        $this->assertEquals(100, $money->amount);
     }
     
 }
